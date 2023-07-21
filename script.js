95b74c253e711ca2890f08fef974a233029ba8d4
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-21 12:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	  let x = 1, y = 1, z = x + y;
+	  for (let i = 3; i <= num; i++) {
+	    x = y;
+	    y = z;
+	    z = x + y;
+	  }
+	  return z;
 }
 
 module.exports = fibonacci;
